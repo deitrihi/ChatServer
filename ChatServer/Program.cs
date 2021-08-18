1ce96f042fbccdea2513af1fdbf085a5e37c3ed4
Deitrihi <deitrihi@naver.com>
2021-08-18 23:02:07 +0900

    init

---
+++ ChatServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChatServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Server.ChatServer();
+			Console.ReadLine();
+		}
+	}
+}
